519cdab77ca95518f3d01839241f19c5003893ab
jesterJohnf <jesterJohnf@users.noreply.github.com>
2017-02-03 14:39:04 +0000

    Add files via upload

--- Training-master/TFLJourney-master/TFLJourneyDescriptionLearner/TFLJourney.js
+++ Training-master/TFLJourney-master/TFLJourneyDescriptionLearner/TFLJourney.js
@@ -1,92 +1,206 @@
-// This is my JS for the TFL journey planner
+/*This is my JS for the TFL Journey Planner Practice*/
 
-
-// My variables
+//My Variables
 
 var getDepPC;
 var getArrPC;
+var myList;
+var choiceOption;
+var journeyDuration;
+var hours;
+var minutes;
 
-//This is to use for my switch conditional 
+//End of my Variables
 
-var choiceOption; 
-
-
-// End Of Varaibles
-
-
-
-// User Experience to better help them navigate and use the app
 
 document.getElementById("depPC").focus();
 
 document.getElementById("googleMap").style.display = "none";
 
-// Event Listeners
+
+//My Event Listenners
 
 var getJourneyData = document.getElementById("mySubmit").addEventListener("click", loadMyData, false);
 
-function loadMyData () {
-    
-    // I will go and get the values of the postcodes from the users
+var postCodeSwitch = document.getElementById("mySwitch").addEventListener("click", switchMyPostCode, false);
+
+var routeOption1 = document.getElementById("option1").addEventListener("click", loadMyData, false);
+
+var routeOption2 = document.getElementById("option2").addEventListener("click", option2Data, false);
+
+var routeOption3 = document.getElementById("option3").addEventListener("click", option3Data, false);
+
+//End of My Event Listenners
+
+
+function loadMyData() {
+
     
     getDepPC = document.getElementById("depPC").value;
     getArrPC = document.getElementById("arrPC").value;
     
-    //console.log(getArrPC);
     //console.log(getDepPC);
-    
-    if(getDepPC === "") {
+    //console.log(getArrPC);
+
+    if ( getDepPC === "") {
         
         document.getElementById("depPC").focus();
-        document.getElementById("messageD").innerHTML = "Please enter a valid postcode";
+        document.getElementById("messageD").innerHTML = "Please enter a POSTCODE";
         
         
-    } else if (getArrPC ==="") {
+    } else if ( getArrPC === "") {
         
         document.getElementById("arrPC").focus();
-        document.getElementById("messageA").innerHTML = "Please enter a valid postcode";
-    
+        document.getElementById("messageA").innerHTML = "Please enter a POSTCODE";
+        
+        
     } else {
         
-        var myRequest = new XMLHttpRequest;
+        document.getElementById("messageD").style.display = "none";
+        document.getElementById("messageA").style.display = "none";
+    
+    var myRequest = new XMLHttpRequest;
+    
+    myRequest.open("GET", "https://api.tfl.gov.uk/journey/journeyresults/" + getDepPC + "/to/" + getArrPC, true);
+    
+    myRequest.onload = function() {
         
-        myRequest.open("GET", "https://api.tfl.gov.uk/journey/journeyresults/"+getDepPC+"/to/"+getArrPC, true);
-        
-        myRequest.onload = function() {
+        if (myRequest.readyState == 4 && myRequest.status == 200) {
             
-            if(myRequest.readyState == 4 && myRequest.status == 200) {
-                
-                var myData = JSON.parse(myRequest.responseText);
-                
-                switch (choiceOption) {
-                        
-                        case(1): 
-                        
-                        // JS notation to get the data
+            var myData = JSON.parse(myRequest.responseText);
+
+            myList = document.getElementById("stepsD");
+            myList.innerHTML = "";
+            
+            switch(choiceOption) {
                     
-                        
-                        break;
-                        
-                        case(2);
-                        
-                        
-                        break;
-                        
-                        
-                        default:
-                        
-                        
-                        break;
-                        
-                }
+                    case(1):
+                    
+                    document.getElementById("startDT").innerHTML = "Date: " + myData.journeys[1].startDateTime.replace("T", " | Time: ");
+                    
+                    document.getElementById("arrivalDT").innerHTML = "Date: " + myData.journeys[1].arrivalDateTime.replace("T", " | Time: ");
+                    
+                    journeyDuration = myData.journeys[1].duration;
+
+                    journeyD(journeyDuration);
+                    
+                    for ( var i = 0; i < myData.journeys[1].legs.length; i++) {
                 
+                    myList.innerHTML += "- " + myData.journeys[1].legs[i].instruction.summary + "<br />";
+                    }
+                    
+                    choiceOption = 0;
+                    
+                    break;
+                    
+                    case(2):
+                    
+                    document.getElementById("startDT").innerHTML = "Date: " + myData.journeys[2].startDateTime.replace("T", " | Time: ");
+                    
+                    document.getElementById("arrivalDT").innerHTML = "Date: " + myData.journeys[2].arrivalDateTime.replace("T", " | Time: ");
+                    
+                    journeyDuration = myData.journeys[2].duration;
+
+                    journeyD(journeyDuration);
+                    
+                    for ( var i = 0; i < myData.journeys[2].legs.length; i++) {
                 
-                //console.log(myData);
+                    myList.innerHTML += "- " + myData.journeys[2].legs[i].instruction.summary + "<br />";
+                    }
+                    
+                    choiceOption = 0;
+                    
+                    break;
+                    
+                    
+                default:
+              
+                    document.getElementById("startDT").innerHTML = "Date: " + myData.journeys[0].startDateTime.replace("T", " | Time: ");
+                    
+                    document.getElementById("arrivalDT").innerHTML = "Date: " + myData.journeys[0].arrivalDateTime.replace("T", " | Time: ");
+                    
+                    journeyDuration = myData.journeys[0].duration;
+                    
+                    journeyD(journeyDuration);
+
+                    for ( var i = 0; i < myData.journeys[0].legs.length; i++) {
+                
+                    myList.innerHTML += "- " + myData.journeys[0].legs[i].instruction.summary + "<br />";
+                        
+                    }
+                    
+                    break;
             }
+            
+                    document.getElementById("googleMap").style.display = "block";
+            
+                    document.getElementById("myGoogleMap").setAttribute("src", "https://www.google.com/maps/embed/v1/directions?key=AIzaSyDqwEMrYspWHOIjkw2EIX9vGwpb8IsL_o8&origin="+getDepPC+"&destination="+getArrPC+"&mode=driving");
+
+            }
+            
         }
+            
+            
+ 
+    myRequest.onerror = function() {
         
-        myRequest.send();
+        document.getElementById("messageD").innerHTML = "You are not connected online and can't reach the server!";
     }
     
-}
+    myRequest.send();
     
+        }
+   
+}
+
+function switchMyPostCode() {
+    
+    //alert("Yes is working!");
+    
+    getDepPC = document.getElementById("depPC").value;
+    getArrPC = document.getElementById("arrPC").value;
+    
+    document.getElementById("depPC").value = getArrPC;
+    document.getElementById("arrPC").value = getDepPC;
+    
+    //console.log(getDepPC);
+    //console.log(getArrPC);
+}
+
+function option2Data() {
+    
+    choiceOption = 1;
+    
+    loadMyData();
+    
+}
+
+function option3Data() {
+    
+    choiceOption = 2;
+    
+    loadMyData();
+    
+}
+
+function journeyD(journeyDuration){
+  hours = Math.trunc(journeyDuration/60);
+  minutes = journeyDuration % 60;
+  document.getElementById("journeyD").innerHTML = + hours + ":" + minutes + " h/m";
+}
+
+/*function reverseDate(str){
+   return str.split('-').reverse().join('-');
+}*/
+
+
+
+
+//Hide email address JS
+
+var myEmail = document.createElement("span");
+
+myEmail.innerHTML = "";
+
+document.getElementById("hideEmail").appendChild(myEmail);
+document.getElementById("hideEmail").setAttribute("href","mailto:manuelc@justit.co.uk");
